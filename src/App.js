45f131386ffb3c4257e0feff4cb6e3089760c70c
lucas-caminha <lucas_santoscaminha@hotmail.com>
2023-04-07 10:51:03 -0300

    Criando components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/HelloWorld';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
+
+  const name = 'Caminha';
+
+  function soma(a, b) {
+    return a + b;
+  }
+
+  const url = 'https://via.placeholder.com/150';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld/>
+      <p>Olá, {name}</p>
+      <p>{soma(3,5)}</p>
+      <img src={url} alt="Minha imagem"/>
     </div>
   );
 }
