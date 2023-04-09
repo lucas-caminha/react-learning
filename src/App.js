28a6c2b9f3ca4d1666b35da90726e3c54b52d610
lucas-caminha <lucas_santoscaminha@hotmail.com>
2023-04-09 20:16:24 -0300

    Trabalhando com css no react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <SayMyName nome={name}/>
       <Pessoa nome="Caminha" idade="23" profissao="Programador" foto="https://via.placeholder.com/150"/>
+      <Frase/>
     </div>
   );
 }
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,7 +1,9 @@
+import styles from './Frase.module.css';
+
 function Frase() {
     return (
-        <div>
-            <p>Componente de frase.</p>
+        <div className={styles.fraseContainer}>
+            <p className={styles.fraseContent}>Componente de frase.</p>
         </div>
     );
 }
