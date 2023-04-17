c05d1017c982afbfe3e81588df1d95d4642ce3ed
lucas-caminha <lucas_santoscaminha@hotmail.com>
2023-04-17 19:49:38 -0300

    Avançando em Props

--- src/components/List.js
+++ src/components/List.js
@@ -5,9 +5,10 @@ function List() {
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Fiat"/>
-                <Item marca="Ferrari"/>
-                <Item marca="Renault"/>
+                <Item marca="Fiat" anoLancamento={1999}/>
+                <Item marca="Ferrari" anoLancamento={2002}/>
+                <Item marca="Renault" anoLancamento={2008}/>
+                <Item/>
             </ul>
         </>
     )
